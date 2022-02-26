10782093c2533ecf6c4e43f421658778f9e24bc2
Lucas Alexandre Zick <lucasalexandrezick@gmail.com>
2022-02-25 21:24:04 -0300

    Update HP50G.py

--- HP50G.py
+++ HP50G.py
@@ -1,5 +1,4 @@
-# V = D / T
-# [+, -, *, /]
+
 def isInc(value):
     try:
         float(value)
@@ -7,30 +6,32 @@ def isInc(value):
     except ValueError:
         return True
 
+
 opposite_ops = {
-    "+":"-",
-    "-":"+",
-    "*":"/",
-    "/":"*"
+    "+": "-",
+    "-": "+",
+    "*": "/",
+    "/": "*"
 }
 
+
 def calculate(calc):
-    #encontrar o = e dividir em 2 partes
+    # Find the '='
     parts = calc.split("=")
-    
-    #encontrar o operador na parte 2
+
+    # Find the operator on the second part
     ex = parts[1]
     valid_op = ['+', '-', '*', '/']
     for op in valid_op:
         values = ex.split(op)
-        if len(values)>1:
+        if len(values) > 1:
             operator = op
             break
     values = [v.strip() for v in values]
 
     leftSide = parts[0].strip()
- 
-    #decidir que operação será feita
+
+    # Choose the operation that will execute
     if isInc(leftSide):
         A = float(values[0])
         B = float(values[1])
@@ -48,8 +49,8 @@ def calculate(calc):
             op = opposite_ops[op]
             A = float(leftSide)
             B = float(values[0])
-    
-    #retornar o valor
+
+    # Return the unknown number's value
     if op == '+':
         return A+B
     elif op == '-':
@@ -59,14 +60,17 @@ def calculate(calc):
     elif op == '/':
         return A/B
 
+
 if __name__ == '__main__':
     stop = 1
     while stop != 0:
         print('--------------------------------------------------------------------------------------------------------------')
-        str = input('\nWrite the expression in the "A = B op C" containing an unknown number at some position\n\n                 ')
+        str = input(
+            '\nWrite the expression in the "A = B op C" containing an unknown number at some position\n\n                 ')
         print('--------------------------------------------------------------------------------------------------------------')
-        print('\nThe unknown number is ',calculate(str), '\n')
+        print('\nThe unknown number is ', calculate(str), '\n')
         print('--------------------------------------------------------------------------------------------------------------')
-        stop = int(input('\nDo you want to try another expression?\n\n                 0 - No\n    Something else - Yes\n\n                 '))
+        stop = int(input(
+            '\nDo you want to try another expression?\n\n                 0 - No\n    Something else - Yes\n\n                 '))
         print('--------------------------------------------------------------------------------------------------------------')
     print('--------------------------------------------------------------------------------------------------------------\n\n                 FINISHED THE EXECUTION')
